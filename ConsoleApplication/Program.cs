ca654639fb5cb863d342d38d7e2c0b44ee2a2309
Vasili Isakau <Vasili_Isakau@epam.com>
2020-11-16 18:55:32 +0300

    First commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Homework
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// The main class of programm.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main program method.
+        /// </summary>
+        /// <param name="args">The list of arguments.</param>
+        public static void Main(string[] args)
+        {
+            var nameIndex = 0;
+
+            // Test if input arguments were supplied.
+            if (!args.Any() || args == null)
+            {
+                var output = $" You didn't enter your name.{Environment.NewLine} Hello, unknown user!";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(output);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Hello, {args[nameIndex]}!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Name(object sender, RoutedEventArgs e)
+        {
+            OutputLabel.Visibility = Visibility.Visible;
+
+            if (string.IsNullOrEmpty(NameBox.Text))
+            {
+                OutputLabel.Content = "«Hello, unknown user!»";
+            }
+            else
+            {
+                OutputLabel.Content = $"«Hello, {NameBox.Text}!»";
+            }
+        }
+    }
+}
